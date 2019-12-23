008eadc6858ea36053e4e47e7ee2f5c6d53b98b3
lavanyak2020 <lavanyakcse2020@gmail.com>
2019-12-23 14:31:37 +0530

    [lavanya] Making executable jar with packages

---
+++ MyProject/src/com/headfirstjava/PackageExercise.java
@@ -0,0 +1,8 @@
+package com.headfirstjava;
+import javax.swing.*;
+import java.util.*;
+public class PackageExercise { // life-altering code here
+	public static void main(String args[]){
+		System.out.println("Lavanya.........");
+	}
+}
